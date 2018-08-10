323f60a11ab3aa49ee9dce1f19051b263b1af34c
Turla <Eliseo.Turla@ingrammicro.com>
2018-08-10 09:39:25 +0800

    view for coupon code

--- app/Http/Controllers/CouponsController.php
+++ app/Http/Controllers/CouponsController.php
@@ -23,7 +23,7 @@ class CouponsController extends Controller
      */
     public function create()
     {
-        //
+        
     }
 
     /**
@@ -34,7 +34,7 @@ class CouponsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        return 'hello';
     }
 
     /**
--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -154,6 +154,15 @@
                                                 </tr>
                                             </tbody>
                                         </table>
+
+                                        <a href="#" class ="have-code">Have a Code?</a>
+                                        <div class="have-code-container">
+                                            <form action="{{route('coupon.store')}}" method="POST">
+                                                    {{csrf_field()}}
+                                                    <input type="text" name="coupon_code" id ="coupon_code">
+                                                    <button type="submit" class ="btn button-plain">Apply</button>
+                                            </form>
+                                        </div>
                                     
                                 </div>
                             </div>
